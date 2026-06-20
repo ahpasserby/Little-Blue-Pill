3db4281bb774eef7d777a61f2716376349a69291
ahpasserby <3537124715@qq.com>
2026-06-21 00:42:42 +0800

    fix: 修复了example 的 bug

--- example/example_F103_LED/Core/Src/gpio.c
+++ example/example_F103_LED/Core/Src/gpio.c
@@ -47,6 +47,7 @@ void MX_GPIO_Init(void)
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(Blue_Pill_LED_GPIO_Port, Blue_Pill_LED_Pin, GPIO_PIN_SET);
